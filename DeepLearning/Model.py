3956d2d1b7f7772916a8426df73dc322c4a2595d
Rakshit Patel <rakshitp44@yahoo.com>
2021-05-21 12:56:51 -0600

    created ccn model

---
+++ DeepLearning/Model.py
@@ -0,0 +1,134 @@
+#from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, Dropout, BatchNormalization, MaxPooling2D, Conv2DTranspose, UpSampling2D
+
+import pickle
+import numpy as np
+import cv2
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+
+class Model:
+
+    def __init__(self, layers = [
+        # Normalizes incoming inputs. First layer needs the input shape to work
+        BatchNormalization(input_shape=(80, 160, 3)),
+        #Dense(units=6, activation='relu'),
+        #Dense(units=6, activation='relu'),
+        Conv2D(8, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv1'),
+
+        # # Conv Layer 2
+        Conv2D(16, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv2'),
+        Dropout(0.2),
+        Conv2D(32, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Conv3'),
+        
+        Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv1'),
+        # Dropout(0.2),
+
+        # # Deconv 2
+        Conv2DTranspose(64, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv2'),
+        Dropout(0.2),
+
+        # # Upsample 2
+        # UpSampling2D(size=(2,2)),
+
+        # # Deconv 3
+        Conv2DTranspose(1, (3, 3), padding='valid', strides=(1,1), activation = 'relu', name = 'Deconv3'),
+        # Dropout(0.2),
+        
+        
+        ]
+        #Dense(units=4, activation='sigmoid')]
+        ):
+        
+
+
+
+
+
+        self._model = Sequential(layers)
+
+    def get_model(self):
+        return self._model
+
+    def train_model(self, batch_size = 128, epochs = 1, input_shape = (480, 640, 3), training_image_path='training_images.p', labeled_image_path='labeled_images.p'):
+        x_train, x_val, y_train, y_val = self.get_training_and_validation_data(training_image_path, labeled_image_path)
+
+        self._model.compile(optimizer='Adam', loss='mean_squared_error')
+        self._model.fit(
+            x_train, y_train, 
+            batch_size=batch_size, 
+            steps_per_epoch=len(x_train)/batch_size, 
+            epochs=epochs, 
+            validation_data=(x_val, y_val), 
+        )
+
+        self._model.save('saved_model.h5')
+
+
+
+    def get_training_and_validation_data(self, training_image_path, labeled_image_path):
+        pickled_training_data = open(training_image_path, 'rb')
+        training_images = pickle.load(pickled_training_data)
+
+        pickled_labeled_data = open(labeled_image_path, 'rb')
+        labeled_images = pickle.load(pickled_labeled_data)
+        
+        training_images = np.array(training_images)
+        labeled_images = np.array(labeled_images)
+
+        # for i in range(len(training_images)):
+        #     training_images[i] = self.resize_image(training_images[i])
+
+        # for i in range(len(labeled_images)):
+        #     labeled_images[i] = self.resize_image(labeled_images[i])
+
+
+
+        normalized_labeled_images = labeled_images / 255
+
+        # Shuffle images along with their labels, then split into training/validation sets
+        training_images, normalized_labeled_images = shuffle(training_images, normalized_labeled_images)
+        #print("Train Images/Labels: ", training_images, normalized_labeled_images)
+        # Test size may be 10% or 20%
+        x_train, x_val, y_train, y_val = train_test_split(training_images, normalized_labeled_images, test_size=0.1)
+        #print("x_train, x_val, y_train, y_val: ", x_train, x_val, y_train, y_val)
+
+        return (x_train, x_val, y_train, y_val)
+    
+        
+    def resize_image(self, image):
+        # Get image ready for feeding into model
+        resized_image = cv2.resize(image, (80, 160))
+        resized_image = np.array(resized_image)
+        #resized_image = resized_image[None,:,:,:]
+        return resized_image
+
+
+
+
+
+def main():
+
+    
+    cnn_model = Model()
+
+    cnn_model.train_model(epochs=1,input_shape=(80, 160, 3))
+    
+    # Freeze layers since training is done
+    cnn_model.trainable = False
+    # cnn_model.compile(optimizer='Adam', loss='mean_squared_error')
+
+    # Save model architecture and weights
+    # cnn_model.save('cnn_model.h5')
+
+    # # Show summary of model
+    # cnn_model.summary()
+    
+    # cnn_model.save()
+
+if __name__ == "__main__":
+    main()
+    
--- LaneDetector.py
+++ LaneDetector.py
@@ -2,10 +2,12 @@ import cv2
 import matplotlib.pyplot as plot
 import numpy as np
 
-from HelperClasses import ImageReader
-from HelperClasses import ImageMasker
-from HelperClasses import ImageWarper
-from HelperClasses import ImageThresholder
+from ImageProcessing import ImageReader
+from ImageProcessing import ImageMasker
+from ImageProcessing import ImageWarper
+from ImageProcessing import ImageThresholder
+
+from DeepLearning import Model
 
 class LaneDetector:
     
@@ -15,17 +17,23 @@ class LaneDetector:
         # Read image
         self.image = self.image_reader.read_image()
 
+
+
         self.height = self.image.shape[0]
         self.width = self.image.shape[1]
-        self.transformed_dimensions = [self.width, self.height]
+        self.image_dimensions = [self.width, self.height]
+
+        print("Image Dimensions: ", self.image_dimensions)
          
         self.region_of_interest = [[int(0.46*self.width),int(0.54*self.height)],[int(0.52*self.width),int(0.54*self.height)],[int(0.80*self.width),int(1*self.height)], [int(0.13*self.width),int(1*self.height)]]
         self.image_masker = ImageMasker.ImageMasker(self.region_of_interest, self.image)
 
 
-        self.image_warper = ImageWarper.ImageWarper(self.transformed_dimensions)
+        self.image_warper = ImageWarper.ImageWarper(self.image_dimensions)
 
         self.image_thresholder = ImageThresholder.ImageThresholder()
+
+        self.cnn_model = Model.Model()
 
     
     def plot_gray_image(self, image):
@@ -38,24 +46,27 @@ class LaneDetector:
 
     def get_warped_image(self):
         masked_image = self.get_masked_image()
-        perspective_matrices = self.image_warper.get_perspective_transform(self.image, self.region_of_interest, self.transformed_dimensions)
+        perspective_matrices = self.image_warper.get_perspective_transform(self.image, self.region_of_interest, self.image_dimensions)
         warped_image = self.image_warper.warp_image(masked_image)
         self.plot_gray_image(warped_image)
         return warped_image
 
     def get_thresholded_image(self):
         warped_image = self.get_warped_image()
-        thresholded_img = self.image_thresholder.adaptive_threshold(warped_image, 255, 1001, -25)
-        self.plot_gray_image(thresholded_img)
+        thresholded_image = self.image_thresholder.adaptive_threshold(warped_image, 255, 1001, -25)
+        self.plot_gray_image(thresholded_image)
         return thresholded_image
 
 
-    def slinding_window(self):
+    def load_training_data(self):
         pass
 
 
+    def load_labeled_data(self):
+        pass
 
-
+    def train_model(self, model, epochs, batch, input_shape):
+        pass
 
 
 
